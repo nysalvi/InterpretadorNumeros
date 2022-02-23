10bb29965b7d25650f3b3ae71cda040652272b63
Ântony Salvi <nysalvi20@gmail.com>
2022-02-22 23:42:07 -0300

    Add project files.

---
+++ InterpretadorNumeros.ConsoleApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+namespace InterpretadorNumeros
+{
+    internal class Program
+    {
+        static string[] base10 = new string[10] {
+        " __ \n|  |\n|__|", "    \n   |\n   |", " __ \n __|\n|__ ", " __ \n __|\n __|",
+        "    \n|__|\n   |", " __ \n|__ \n __|", " __ \n|__ \n|__|", " __ \n   |\n   |",
+        " __ \n|__|\n|__|", " __ \n|__|\n __|"};
+        static string separaNumero(ref string[] linhas)
+        {
+            string numeroAtual = "";
+            numeroAtual += linhas[0].Substring(0, 4) + "\n";
+            numeroAtual += linhas[1].Substring(0, 4) + "\n";
+            numeroAtual += linhas[2].Substring(0, 4);
+
+            linhas[0] = linhas[0].Remove(0, 4);
+            linhas[1] = linhas[1].Remove(0, 4);
+            linhas[2] = linhas[2].Remove(0, 4);
+            return numeroAtual;
+        }
+        static string[] removeEspaços(ref StringReader leitor, ref string numeros)
+        {
+            int tamanho = 1;
+            foreach (char x in numeros)
+            {
+                if (x == '\n')
+                    tamanho++;
+            }
+            string[] copia = new string[tamanho];
+            int i = 0;
+            while (i < tamanho)
+            {
+                if (i == 0 || i % 4 != 0)
+                {
+                    copia[i] = leitor.ReadLine();
+                    copia[i] = copia[i].Substring(copia[i].Length - 40, 40);
+                }
+                i++;
+            }
+            return copia;
+        }
+        static void Main(string[] args)
+        {
+            string numero = @" __      __  __      __  __  __  __  __ 
+                              |  |   | __| __||__||__ |__    ||__||__|
+                              |__|   ||__  __|   | __||__|   ||__| __|";
+            /*       30 espaços       */
+            StringReader stringReader = new StringReader(numero);
+            string[] linhas = removeEspaços(ref stringReader, ref numero);
+            string numeroAtual;
+            int iterador = 0;
+            int linha = 0;
+            while (linha <= linhas.Length)
+            {
+                numeroAtual = separaNumero(ref linhas);
+                Console.Write(Array.IndexOf(base10, numeroAtual));
+                iterador += 4;
+                if (iterador >= linhas[0].Length)
+                {
+                    iterador = 0;
+                    linha++;
+                }
+            }
+        }
+    }
+}
---
+++ Tentativa2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+namespace Tentativa2
+{
+    internal class Program
+    {
+        static string[][] _linha = new string[3][]{ new string[10]{" __ ", "    ", " __ ", " __ ",
+                "    ", " __ ", " __ ", " __ ", " __ ", " __ "},
+            new string[10]{"|  |", "   |", " __|", " __|",
+                "|__|", "|__ ", "|__ ", "   |", "|__|", "|__|"},
+            new string[10]{"|__|" , "   |" , "|__ " , " __|",
+                "   |", " __|",  "|__|", "   |", "|__|", " __|"}};
+        static string[] calculaLinha(int linhaAtual, ref string[] linha, ref string[] fila)
+        {
+            string itera;
+            itera = linha[linhaAtual][0].ToString();
+            itera += linha[linhaAtual][1];
+            itera += linha[linhaAtual][2];
+            itera += linha[linhaAtual][3];
+            linha[linhaAtual] = linha[linhaAtual].Remove(0, 4);
+            for (int i = 0; i < fila.Length; i++)
+            {
+                if (fila[i] is not null && !fila[i].Equals(itera))
+                {
+                    fila[i] = null;
+                }
+                else if (linhaAtual < 2)
+                {
+                    fila[i] = _linha[linhaAtual + 1][i];
+                }
+            }
+            return fila;
+        }
+        static void Main(string[] args)
+        {
+            string numero = @" __      __  __      __  __  __  __  __ 
+                              |  |   | __| __||__||__ |__    ||__||__|
+                              |__|   ||__  __|   | __||__|   ||__| __|";
+            StringReader stringReader = new StringReader(numero);
+            string[] linhas = new string[3] { stringReader.ReadLine(), stringReader.ReadLine().Trim(),
+            stringReader.ReadLine().Trim() };
+            string[] fila;
+            int valor;
+            for (int i = 0; i < 10; i++)
+            {
+                fila = _linha[0];
+                //Array.ForEach(fila, x=> Console.Write(x));
+                //Array.ForEach(_linha[0], x => Console.Write(x));
+                calculaLinha(0, ref linhas, ref fila);
+                calculaLinha(1, ref linhas, ref fila);
+                calculaLinha(2, ref linhas, ref fila);
+                valor = Array.IndexOf(fila, Array.Find(fila, x => x is not null));
+                Console.Write(valor);
+            }
+        }
+    }
+}
